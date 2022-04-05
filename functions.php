cb7e18f55b6e6264ed9b5338812e4fd0842e2d6d
Menn <m@seedwebs.com>
2022-04-05 18:24:36 +0700

    Basic Template with Noto Sans Thai

--- functions.php
+++ functions.php
@@ -10,46 +10,36 @@
 
 
 if ( ! function_exists( 'din_support' ) ) :
-
 	function din_support() {
 		add_theme_support( 'wp-block-styles' );
 		add_editor_style( 'assets/css/wp-editor.css' );
 	}
-
 endif;
-
 add_action( 'after_setup_theme', 'din_support' );
 
 if ( ! function_exists( 'din_styles' ) ) :
-
 	function din_styles() {
 		$ver = wp_get_theme()->get( 'Version' );
 		$url = get_template_directory_uri() . '/assets/css/';
-
-		wp_enqueue_style( 'din', $url . 'mobile.css', array(), $ver );
-		
-
+		wp_enqueue_style( 'din-mobile', $url . 'mobile.css', array(), $ver );
+		wp_enqueue_style('din-desktop', $url . 'desktop.css', array(), $ver , '(min-width: 992px)');
 	}
-
 endif;
-
 add_action( 'wp_enqueue_scripts', 'din_styles' );
 
-if ( ! function_exists( 'din_editor_styles' ) ) :
-
-	/**
-	 * Enqueue editor styles.
-	 *
-	 * @since Livro 1.0
-	 *
-	 * @return void
-	 */
-	function din_editor_styles() {
-
-
-
-	}
-
-endif;
-
-add_action( 'admin_init', 'din_editor_styles' );
+//  Remove "Category: ", "Tag: ", "Taxonomy: " from archive title
+function dim_get_the_archive_title($title) {
+    if (is_category()) {
+        $title = single_cat_title('', false);
+    } elseif (is_tag()) {
+        $title = single_tag_title('', false);
+    } elseif (is_post_type_archive()) {
+        $title = post_type_archive_title('', false);
+    } elseif (is_author()) {
+        $title = '<span class="vcard">' . get_the_author() . '</span>';
+    } elseif (is_tax()) {
+        $title = single_term_title('', false);
+    }
+    return $title;
+}
+add_filter('get_the_archive_title', 'dim_get_the_archive_title');
